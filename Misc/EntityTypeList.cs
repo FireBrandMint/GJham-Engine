b8e426798a0e2e9420a37b8e0640b05cd6d1a95d
guliver-jham <62035564+guliver-jham@users.noreply.github.com>
2022-07-02 16:13:46 -0300

    Preparations for a prefab system AGAIN.

--- Misc/EntityTypeList.cs
+++ Misc/EntityTypeList.cs
@@ -6,14 +6,23 @@ using System.Reflection;
 
 using System.Linq;
 
-delegate Entity ETypeCreate ();
+using System.Diagnostics;
+
+delegate Entity ETypeCreate (ByteReader reader, int propertyCount);
 
 public class EntityTypeList
 {
     //https://stackoverflow.com/questions/752/how-to-create-a-new-object-instance-from-a-type
 
+    public static void InstanceEntityType(String typeName, ByteReader reader)
+    {
+        EntityInstancers[typeName].Invoke(reader, reader.ReadInt32());
+    }
+
     public static void ListAll()
     {
+        Stopwatch watch = Stopwatch.StartNew();
+
         string currFile = Directory.GetCurrentDirectory() + @"\Misc\EntityTypeList.cs";
 
         string fileText = File.ReadAllText(currFile);
@@ -40,14 +49,53 @@ public class EntityTypeList
 
             if (typeName == entityName) continue;
 
-            PropertyInfo[] properties = type.GetProperties(
+            FieldInfo[] fields = type.GetFields(
                 BindingFlags.Instance|
                 BindingFlags.Public
             );
 
             list += $"\n        {{\"{typeName}\", Instance{typeName}}},";
 
-            fileText += $"\n    private static Entity Instance{typeName} () => new {typeName}();";
+            fileText += $"\n    private static Entity Instance{typeName} (ByteReader reader, int propertyCount)\n    {{\n        {typeName} entity = new {typeName}();";
+
+            fileText += "\n        for(int i =0; i< propertyCount; ++i) { int fieldHashcode = reader.ReadInt32(); switch(fieldHashcode){";
+
+            for(int i2 = 0; i2 < fields.Length; ++i2)
+            {
+                String valueMethod;
+
+                FieldInfo field = fields[i2];
+
+                if (field.IsInitOnly) continue;
+
+                if (AcceptedTypesOut.TryGetValue(field.GetType(), out valueMethod))
+                {
+                    fileText+= $"case {field.Name.GetHashCode()}: entity.{field.Name} = {valueMethod}; break;";
+                }
+            }
+
+            PropertyInfo[] properties = type.GetProperties(
+                BindingFlags.Instance|
+                BindingFlags.Public
+            );
+
+            for(int i2 = 0; i2 < properties.Length; ++i2)
+            {
+                String valueMethod;
+
+                PropertyInfo property = properties[i2];
+
+                if (!property.CanWrite) continue;
+
+                if (AcceptedTypesOut.TryGetValue(property.PropertyType, out valueMethod))
+                {
+                    fileText+= $"case {property.Name.GetHashCode()}: entity.{property.Name} = {valueMethod}; break; ";
+                }
+            }
+
+            fileText+="}}";
+
+            fileText+= $"\n        return entity;\n    }}";
         }
 
         list+= "\n    };";
@@ -57,12 +105,26 @@ public class EntityTypeList
         fileText+= "\n}";
 
         File.WriteAllText(currFile, fileText);
+
+        double timeTook = (double)watch.ElapsedTicks / ((double)Stopwatch.Frequency / 1000.0);
+
+        Console.WriteLine($"Took {timeTook}MS to map entity instancers to EntityTypeList class.");
+
+        watch.Stop();
     }
 
-    public static Entity GetEntity(String className)
+    private static Dictionary<Type, String> AcceptedTypesOut  = new Dictionary<Type, string>()
     {
-        return  EntityInstancers[className].Invoke();
-    }
+        {typeof(Int32), "reader.ReadInt32()"},
+        {typeof(Int64), "reader.ReadInt64()"},
+        {typeof(Single), "reader.ReadFloat()"},
+        {typeof(Double), "reader.ReadDouble()"},
+        {typeof(Char), "reader.ReadByte()"},
+        {typeof(String), "reader.ReadString()"},
+        {typeof(Byte), "reader.ReadByte()"},
+        {typeof(Boolean), "reader.ReadBool()"},
+        {typeof(FInt), "reader.ReadFInt()"},
+    };
 
     //https://stackoverflow.com/questions/26733/getting-all-types-that-implement-an-interface
 
@@ -81,7 +143,12 @@ public class EntityTypeList
     }
 
     //Activation methods!
-    private static Entity InstanceDTestLineProvider () => new DTestLineProvider();
+    private static Entity InstanceDTestLineProvider (ByteReader reader, int propertyCount)
+    {
+        DTestLineProvider entity = new DTestLineProvider();
+        for(int i =0; i< propertyCount; ++i) { int fieldHashcode = reader.ReadInt32(); switch(fieldHashcode){case 1086275545: entity.IsDestroyed = reader.ReadBool(); break; case 1255664404: entity.CantProcess = reader.ReadBool(); break; case -1649701350: entity.ZValue = reader.ReadInt32(); break; }}
+        return entity;
+    }
 
     private static Dictionary<String, ETypeCreate> EntityInstancers = new Dictionary<string, ETypeCreate>()
     {
